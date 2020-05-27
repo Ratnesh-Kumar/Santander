538604b002e9b355a5a5f261f139cd57d76b34a3
Shefali Gupta <shefali.gupta@officedepot.com>
2020-05-27 19:13:55 +0530

    Schemas Added IOS
    Code Added

--- ios/Santander/AppDelegate.m
+++ ios/Santander/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "ReactNativeConfig.h"
 
 #if DEBUG
 #import <FlipperKit/FlipperClient.h>
@@ -38,6 +39,8 @@ static void InitializeFlipper(UIApplication *application) {
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
+
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
