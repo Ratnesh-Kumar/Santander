d247116b4a9ee466fb8b10a16436ee9cb3a9bce1
Shefali Gupta <shefali.gupta@officedepot.com>
2020-05-27 13:25:47 +0530

    Revert "Added react-native-config"
    This reverts commit cedc5697cc5f266a8a3769fde46ad917f742fc71.

--- ios/Santander/AppDelegate.m
+++ ios/Santander/AppDelegate.m
@@ -12,8 +12,6 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
-#import "ReactNativeConfig.h"
-
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -39,8 +37,6 @@ static void InitializeFlipper(UIApplication *application) {
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-  
-  NSDictionary *config = [ReactNativeConfig env];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
