97ae4d5a42e1dbb77d71e3a3f71c324d618e3b37
jaideep012 <Jaideep.singh@officedepot.com>
2020-05-28 15:18:21 +0530

    create design constant

---
+++ src/config/designConstants.js
@@ -0,0 +1,7 @@
+export const textAppearanceSmall = 10
+export const textAppearanceMedium = 14
+export const textAppearanceLarge = 18
+
+export const screenMargin = 20
+export const dialogMargin = 30
+export const screenBottomMargin = 50
