3bb5bc7ffd01a404de5ca4baa82ef5f9735fdc96
jaideep012 <Jaideep.singh@officedepot.com>
2020-06-02 16:42:04 +0530

    second campaign screen done

---
+++ src/scenes/Campaign/createCampaign.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Image, TextInput, ScrollView } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import Header from '../../components/Header';
+import campaignStyle from './campaignStyle';
+import { strings } from '../../i18next/i18n';
+import * as RNLocalize from "react-native-localize";
+// import {RNFirebase, firestore} from 'react-native-firebase';
+import { getTermsAndConditions, getPrivacyPolicy, getRemoteConfig } from '../../config/firebaseFirestore';
+import GlobalData from '../../utils/GlobalData';
+import BaseComponent from '../../BaseComponent';
+import TextInputMaterial from '../../components/textInputMaterial';
+import AppButton from '../../components/AppButton'
+var globalData = new GlobalData();
+var constants = require('../../config/Constants');
+var compaignConstants = require('./campaignConstants')
+var colorConstant = require('../../config/colorConstant')
+export default class CampaignScreen extends BaseComponent {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      campaignName: '',
+    }
+  }
+
+  async componentDidMount() {
+
+  }
+  render() {
+    return (
+      <View style={campaignStyle.container}>
+        <Header title={strings('createCampaign.screenTitle')} isCrossIconVisible={false} />
+        <View style={campaignStyle.viewContainer}>
+          <ScrollView keyboardShouldPersistTaps={'always'}>
+            <AppButton buttonText={strings('createCampaign.nextButtonText')} onButtonPressed={()=>{
+                Actions.createCampaignShare()
+            }}/>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  viewContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'black',
+  },
+});
+
