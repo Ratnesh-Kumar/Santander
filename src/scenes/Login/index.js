82e55dc6583cc091bef5fb6b82007f65bf821302
jaideep012 <Jaideep.singh@officedepot.com>
2020-06-13 18:15:14 +0530

    done

--- src/scenes/Login/index.js
+++ src/scenes/Login/index.js
@@ -49,8 +49,8 @@ export default class LoginView extends BaseComponent {
     this.state = {
       showPass: true,
       press: false,
-      username: 'abc34@yopmail.com',
-      password: 'Tester@123',
+      username: '',
+      password: '',
       isTouchIdSupported: false,
       isFaceIdSupported: false,
       googleUserInfo: '',
