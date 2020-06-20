8bd31f9ad3f1258dd036cad29b8e2777397476ff
knitratnesh <Ratnesh@2020>
2020-06-21 00:00:16 +0530

    minor UI changes

--- src/scenes/BusinessProfile/index.js
+++ src/scenes/BusinessProfile/index.js
@@ -804,7 +804,7 @@ export default class BusinessProfileView extends BaseComponent {
     getRequestBody(data) {
         console.log("getRequestBody :" + JSON.stringify(data))
         return {
-            "trackInventory": false, 
+            "trackInventory": data.trackInventory, 
             "taxOnSales": data.taxOnSales,
             "taxOnPurchase": true,
             "purchaseTaxReclaimable": false,
