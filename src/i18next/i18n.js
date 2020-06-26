a123b65f01181562a25277727fbe215b6306c045
jaideep012 <Jaideep.singh@officedepot.com>
2020-06-26 16:20:55 +0530

    pl.json

--- src/i18next/i18n.js
+++ src/i18next/i18n.js
@@ -4,6 +4,7 @@ import I18n from 'react-native-i18n';
 // Import all locales
 import en from './en.json';
 import es from './es.json';
+import pl from './pl.json';
 
 // Should the app fallback to English if user locale doesn't exists
 I18n.fallbacks = true;
@@ -11,14 +12,15 @@ I18n.fallbacks = true;
 // Define the supported translations
 I18n.translations = {
   en,
-  es
+  es,
+  pl
 };
 
 const currentLocale = I18n.currentLocale();
 console.log("############## currentLocale 1: "+currentLocale)
 
 // Is it a RTL language?
-export const isRTL = currentLocale.indexOf('es') === 0; //|| currentLocale.indexOf('ar') === 0;
+export const isRTL = (currentLocale.indexOf('es') === 0 || currentLocale.indexOf('pl') === 0);
 console.log("############## currentLocale 2: "+(currentLocale.indexOf('es') === 0))
 // Allow RTL alignment in RTL languages
 ReactNative.I18nManager.allowRTL(isRTL);
